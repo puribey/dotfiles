06614556eb7697da4bf7aed88ddcc595300b6aa8
Sergio Rafael Gianazza <sgianazza@gmail.com>
2020-03-12 01:28:36 -0300

    add rust tools

---
+++ install-rust-tools.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+
+set -e
+
+#ls replacement
+cargo install exa
+# ps replacement
+cargo install procs
+# cat replacement
+cargo install bat
+# ripgrep replacement
+cargo install ripgrep
+# nano replacement
+cargo install kibi
+# find replacement
+cargo install fd-find
+# du replacement
+cargo install du-dust
+# hyperfine: benchmarking tool
+cargo install hyperfine
+# ytop - top replacement
+cargo install -f --git https://github.com/cjbassi/ytop ytop
+# bandwhich:
+cargo install bandwhich
+# hexdump replacement
+cargo install hx
+# bingrep
+cargo install bingrep
+# miniserve: simple http server
+cargo +nightly install -f miniserve
+# Generate licenses
+cargo install licensor
